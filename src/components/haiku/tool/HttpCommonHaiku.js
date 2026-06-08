e7148293d8cf7b3b178bd548f08c79e8bf6cdc3c
Simas <simonas.aruna@gmail.com>
2026-06-08 22:44:49 +0300

    master: nginx proxy redirect to backend to solve https mixed content

--- src/components/haiku/tool/HttpCommonHaiku.js
+++ src/components/haiku/tool/HttpCommonHaiku.js
@@ -3,7 +3,8 @@ import axios from "axios";
 const backendUrl =
   window.location.hostname === "localhost"
     ? "http://localhost:8080"       // local dev
-    : "http://129.151.221.35:8080"; // server deployment
+    //: "http://129.151.221.35:8080"; // server deployment (the problem with this is that when site is https, it requires https backend)
+    : "/api"; //nginx config will redirect this to backend, which is in relation to nginx — "http://localhost:8080". Solves HTTPS mixed-content
 
 export default axios.create({
     baseURL: backendUrl,
